4cb11a46ffc41a74baa466ef83fa06a4a8f42288
anilyavas <anil.yavas99@gmail.com>
2024-08-28 13:00:47 +0300

    custom provider and refactoring

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,49 +1,21 @@
 import { Image } from 'expo-image';
-import * as MediaLibrary from 'expo-media-library';
 import { Stack } from 'expo-router';
-import { useEffect, useState } from 'react';
 import { FlatList } from 'react-native';
 
+import { useMedia } from '~/providers/MediaProvider';
+
 export default function Home() {
-  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
-  const [localAssets, setLocalAssets] = useState<MediaLibrary.Asset[]>([]);
-  const [hasNextPage, setHasNextPage] = useState(true);
-  const [endCursor, setEndCursor] = useState<string>();
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    if (permissionResponse?.status !== 'granted') {
-      requestPermission();
-    }
-  }, []);
-
-  useEffect(() => {
-    if (permissionResponse?.status === 'granted') {
-      loadLocalAssets();
-    }
-  }, [permissionResponse]);
-
-  const loadLocalAssets = async () => {
-    if (loading || !hasNextPage) {
-      return;
-    }
-    setLoading(true);
-    const assetsPage = await MediaLibrary.getAssetsAsync({ after: endCursor });
-    setLocalAssets((existingItems) => [...existingItems, ...assetsPage.assets]);
-    setHasNextPage(assetsPage.hasNextPage);
-    setEndCursor(assetsPage.endCursor);
-    setLoading(false);
-  };
+  const { assets, loadLocalAssets } = useMedia();
 
   return (
     <>
       <Stack.Screen options={{ title: 'Photos' }} />
       <FlatList
-        data={localAssets}
+        data={assets}
         columnWrapperClassName="gap-[2px]"
         contentContainerClassName="gap-[2px]"
         onEndReached={loadLocalAssets}
-        refreshing={loading}
+        //refreshing={loading}
         onEndReachedThreshold={1}
         numColumns={4}
         renderItem={({ item }) => (
