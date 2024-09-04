93857dd54ef95036dfcdf4a77e11ea52b19e8c54
anilyavas <anil.yavas99@gmail.com>
2024-09-04 14:59:33 +0300

    Upload files to supabase storage

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -5,10 +5,6 @@ import { Alert, View, AppState, Button, TextInput } from 'react-native';
 import { useAuth } from '~/providers/AuthProvider';
 import { supabase } from '~/utils/supabase';
 
-// Tells Supabase Auth to continuously refresh the session automatically if
-// the app is in the foreground. When this is added, you will continue to receive
-// `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
-// if the user's session is terminated. This should only be registered once.
 AppState.addEventListener('change', (state) => {
   if (state === 'active') {
     supabase.auth.startAutoRefresh();
--- providers/MediaProvider.tsx
+++ providers/MediaProvider.tsx
@@ -1,15 +1,24 @@
+import { decode } from 'base64-arraybuffer';
+import * as FileSystem from 'expo-file-system';
 import * as MediaLibrary from 'expo-media-library';
+import mime from 'mime';
 import { createContext, PropsWithChildren, useContext, useState, useEffect } from 'react';
+
+import { useAuth } from './AuthProvider';
+
+import { supabase } from '~/utils/supabase';
 
 type MediaContextType = {
   assets: MediaLibrary.Asset[];
   loadLocalAssets: () => void;
   getAssetById: (id: string) => MediaLibrary.Asset | undefined;
+  syncToCloud: (asset: MediaLibrary.Asset) => void;
 };
 const MediaContext = createContext<MediaContextType>({
   assets: [],
   loadLocalAssets: () => {},
   getAssetById: () => undefined,
+  syncToCloud: () => {},
 });
 
 export default function MediaContextProvider({ children }: PropsWithChildren) {
@@ -18,6 +27,7 @@ export default function MediaContextProvider({ children }: PropsWithChildren) {
   const [hasNextPage, setHasNextPage] = useState(true);
   const [endCursor, setEndCursor] = useState<string>();
   const [loading, setLoading] = useState(false);
+  const { user } = useAuth();
 
   useEffect(() => {
     if (permissionResponse?.status !== 'granted') {
@@ -47,8 +57,25 @@ export default function MediaContextProvider({ children }: PropsWithChildren) {
     return localAssets.find((asset) => asset.id === id);
   };
 
+  const syncToCloud = async (asset: MediaLibrary.Asset) => {
+    //Uploading photo
+    const info = await MediaLibrary.getAssetInfoAsync(asset);
+    if (!info.localUri || !user) {
+      return;
+    }
+    const base64String = await FileSystem.readAsStringAsync(info.localUri, { encoding: 'base64' });
+    const arrayBuffer = decode(base64String);
+    const { data, error } = await supabase.storage
+      .from('assets')
+      .upload(`${user.id}/${asset.filename}`, arrayBuffer, {
+        contentType: mime.getType(asset.filename) ?? 'image/jpeg',
+        upsert: true,
+      });
+  };
+
   return (
-    <MediaContext.Provider value={{ assets: localAssets, loadLocalAssets, getAssetById }}>
+    <MediaContext.Provider
+      value={{ assets: localAssets, loadLocalAssets, getAssetById, syncToCloud }}>
       {children}
     </MediaContext.Provider>
   );
